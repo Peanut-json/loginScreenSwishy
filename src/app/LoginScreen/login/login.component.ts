106fdefba9674c3f025952224a44cd465082e606
Peanut-json <retroownerxd@gmail.com>
2023-06-23 17:48:52 +0100

    created smooth transition for sign up and in

--- src/app/LoginScreen/login/login.component.ts
+++ src/app/LoginScreen/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+ 
+  @ViewChild('signinbtn') signinbtn!: ElementRef;
+  @ViewChild('signupbtn') signupbtn!: ElementRef;
+  @ViewChild('formbox') formbox!: ElementRef;
+
+
+   
+ ngAfterViewInit() {
+    this.signupbtn.nativeElement.addEventListener('click', () => {
+      this.formbox.nativeElement.classList.add('active');
+    });
+
+    this.signinbtn.nativeElement.addEventListener('click', () => {
+      this.formbox.nativeElement.classList.remove('active');
+    });
+  }
+
+
 }
+
+
